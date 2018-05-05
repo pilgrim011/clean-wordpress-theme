feb80e03f92979cd2093175c3a1e7e10573e4e53
pilgrim011 <printerstampac@gmail.com>
2018-05-05 17:57:42 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * clean functions and definitions
+ *
+ * @package clean
+ */
+ 
+ /**
+  * Store the theme's directory path and uri in constants
+  */
+ define('THEME_DIR_PATH', get_template_directory());
+ define('THEME_DIR_URI', get_template_directory_uri());
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 750; /* pixels */
+
+if ( ! function_exists( 'clean_setup' ) ) :
+/**
+ * Set up theme defaults and register support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function clean_setup() {
+	global $cap, $content_width;
+
+	// Add html5 behavior for some theme elements
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+    // This theme styles the visual editor with editor-style.css to match the theme style.
+	add_editor_style();
+
+	/**
+	 * Add default posts and comments RSS feed links to head
+	*/
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * Enable support for Post Thumbnails on posts and pages
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	*/
+	add_theme_support( 'post-thumbnails' );
+
+	/**
+	 * Enable support for Post Formats
+	*/
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+
+	/**
+	 * Setup the WordPress core custom background feature.
+	*/
+	add_theme_support( 'custom-background', apply_filters( 'clean_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+		) ) );
+	add_theme_support( 'title-tag' );
+	
+	/**
+	 * Make theme available for translation
+	 * Translations can be filed in the /languages/ directory
+	 * If you're building a theme based on clean, use a find and replace
+	 * to change 'clean' to the name of your theme in all the template files
+	*/
+	load_theme_textdomain( 'clean', THEME_DIR_PATH . '/languages' );
+
+	/**
+	 * This theme uses wp_nav_menu() in one location.
+	*/
+	register_nav_menus( array(
+		'primary'  => __( 'Header bottom menu', 'clean' ),
+		) );
+
+}
+endif; // clean_setup
+add_action( 'after_setup_theme', 'clean_setup' );
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ */
+function clean_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'clean' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+		) );
+}
+add_action( 'widgets_init', 'clean_widgets_init' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function clean_scripts() {
+
+	// Import the necessary TK Bootstrap WP CSS additions
+	wp_enqueue_style( 'clean-bootstrap-wp', THEME_DIR_URI . '/includes/css/bootstrap-wp.css' );
+
+	// load bootstrap css
+	wp_enqueue_style( 'clean-bootstrap', THEME_DIR_URI . '/includes/resources/bootstrap/css/bootstrap.min.css' );
+
+	// load Font Awesome css
+	wp_enqueue_style( 'clean-font-awesome', THEME_DIR_URI . '/includes/css/font-awesome.min.css', false, '4.1.0' );
+
+	// load clean styles
+	wp_enqueue_style( 'clean-style', get_stylesheet_uri() );
+
+	// load bootstrap js
+	wp_enqueue_script('clean-bootstrapjs', THEME_DIR_URI . '/includes/resources/bootstrap/js/bootstrap.min.js', array('jquery') );
+
+	// load bootstrap wp js
+	wp_enqueue_script( 'clean-bootstrapwp', THEME_DIR_URI . '/includes/js/bootstrap-wp.js', array('jquery') );
+
+	wp_enqueue_script( 'clean-skip-link-focus-fix', THEME_DIR_URI . '/includes/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'clean-keyboard-image-navigation', THEME_DIR_URI . '/includes/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	}
+
+}
+add_action( 'wp_enqueue_scripts', 'clean_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require THEME_DIR_PATH . '/includes/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require THEME_DIR_PATH . '/includes/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require THEME_DIR_PATH . '/includes/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require THEME_DIR_PATH . '/includes/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require THEME_DIR_PATH . '/includes/jetpack.php';
+
+/**
+ * Load custom WordPress nav walker.
+ */
+require THEME_DIR_PATH . '/includes/bootstrap-wp-navwalker.php';
+
+/**
+ * Adds WooCommerce support
+ */
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
